03b732137e712efbf09979d75f4eaab7096af514
Adam Bien <abien@adam-bien.com>
2019-07-15 15:34:15 +0200

    RULZ dependency removed

--- statustest-st/src/test/java/com/airhacks/statustest/boundary/StatusesResourceIT.java
+++ statustest-st/src/test/java/com/airhacks/statustest/boundary/StatusesResourceIT.java
@@ -2,15 +2,14 @@
  */
 package com.airhacks.statustest.boundary;
 
-import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
-import static com.airhacks.rulz.jaxrsclient.JAXRSClientProvider.buildWithURI;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
-import static com.airhacks.rulz.jaxrsclient.HttpMatchers.statusCode;
 
 /**
  *
@@ -19,98 +18,103 @@ import static com.airhacks.rulz.jaxrsclient.HttpMatchers.statusCode;
 public class StatusesResourceIT {
 
     static final String STATUS_HEADER_PARAM = "status";
+    private WebTarget tut;
 
-    @Rule
-    public JAXRSClientProvider client = buildWithURI("http://localhost:8080/statustest/resources/statuses");
+    @Before
+    public void initClient() {
+        this.tut = ClientBuilder.newClient().target("http://localhost:8080/statustest/resources/statuses");
+    }
+
 
     @Test
     public void get() {
         int expected = 205;
 
-        Response response = client.target().
+        Response response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 get();
         assertThat(response.getStatus(), is(expected));
 
         expected = 203;
-        response = client.target().
+        response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 get();
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response.getStatus(), is(expected));
+
     }
 
     @Test
     public void post() {
         int expected = 205;
 
-        Response response = client.target().
+        Response response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 post(Entity.text(""));
         assertThat(response.getStatus(), is(expected));
 
         expected = 203;
-        response = client.target().
+        response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 post(Entity.text(""));
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response.getStatus(), is(expected));
     }
 
     @Test
     public void put() {
         int expected = 205;
 
-        Response response = client.target().
+        Response response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 put(Entity.text(""));
         assertThat(response.getStatus(), is(expected));
 
         expected = 203;
-        response = client.target().
+        response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 put(Entity.text(""));
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response, is(expected));
     }
 
     @Test
     public void delete() {
         int expected = 205;
 
-        Response response = client.target().
+        Response response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 delete();
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response, is(expected));
 
         expected = 203;
-        response = client.target().
+        response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 delete();
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response, is(expected));
     }
 
     @Test
     public void options() {
         int expected = 205;
 
-        Response response = client.target().
+        Response response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 options();
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response, is(expected));
 
         expected = 203;
-        response = client.target().
+        response = tut.
                 request().
                 header(STATUS_HEADER_PARAM, expected).
                 options();
-        assertThat(response, is(statusCode(expected)));
+        assertThat(response, is(expected));
     }
 
 }
